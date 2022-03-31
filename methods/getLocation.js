637beba833e72c5d7f3a81741513993a78fdac7b
sanjaraybekov <sanjaraliaybekov@gmail.com>
2022-03-31 13:40:16 +0500

    adjanslda

--- methods/getLocation.js
+++ methods/getLocation.js
@@ -1,7 +1,9 @@
 const { bot } = require("../constants/const");
 
-const getLocation = (ctx) => {
+const getLocation = () => {
   return bot.telegram.sendLocation(-1001718670724, "41.3217688", "69.237796");
 };
+
+//977466424; sarvar
 
 module.exports = { getLocation };
