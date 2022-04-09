7c37db59ac7b34cccb71a807f43311821673cc9e
sanjaraybekov <sanjaraliaybekov@gmail.com>
2022-04-09 21:43:04 +0500

    my

--- questions/answerQuestions.js
+++ questions/answerQuestions.js
@@ -5,7 +5,7 @@ function answerQuestions(ctx) {
   let text = ctx.message.text;
   if (text.includes("manzil" || "Manzil" || "MANZIL")) {
     return getLocation(ctx);
-  } else if (text.includes("kitobi bormi")) {
+  } else if (text.includes("kitobi bormi" || "kitobi bo'shmi")) {
     return getSearchBooks(ctx);
   }
 }
